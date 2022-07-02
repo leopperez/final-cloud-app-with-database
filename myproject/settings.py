d2c1e102e429f072c924e2d13ddb2b22abd6ff74
leopperez <55115351+leopperez@users.noreply.github.com>
2022-07-01 21:03:06 -0500

    Temp

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'MTE1NDEtcDEyYW5r',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
